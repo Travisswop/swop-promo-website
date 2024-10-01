1987496e1d2d3bcfb9cbb0ddffc97f51cde23a84
arsahak <arsahak99@gmail.com>
2024-10-01 18:10:16 +0600

    update software page

--- components/hardware/ConectMobileSection.jsx
+++ components/hardware/ConectMobileSection.jsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import React, {
   useState,
@@ -6,18 +6,18 @@ import React, {
   useRef,
   useCallback,
   useMemo,
-} from 'react';
-import Image from 'next/image';
-import { motion } from 'framer-motion';
+} from "react";
+import Image from "next/image";
+import { motion } from "framer-motion";
 
-import SectionLayout from '../shared/SectionLayout';
-import ScrollMotionEffect from '../motion/ScrollMotionEffect';
+import SectionLayout from "../shared/SectionLayout";
+import ScrollMotionEffect from "../motion/ScrollMotionEffect";
 
-import { TiBusinessCard } from 'react-icons/ti';
-import { IoIosWatch } from 'react-icons/io';
-import { GrTransaction } from 'react-icons/gr';
-import { IoRestaurantOutline, IoCodeSharp } from 'react-icons/io5';
-import { RiChatSettingsLine } from 'react-icons/ri';
+import { TiBusinessCard } from "react-icons/ti";
+import { IoIosWatch } from "react-icons/io";
+import { GrTransaction } from "react-icons/gr";
+import { IoRestaurantOutline, IoCodeSharp } from "react-icons/io5";
+import { RiChatSettingsLine } from "react-icons/ri";
 
 const ConectMobileSection = () => {
   const [hoveredItem, setHoveredItem] = useState(null);
@@ -33,26 +33,26 @@ const ConectMobileSection = () => {
       {
         id: 1,
         icon: TiBusinessCard,
-        title: 'Business Cards',
-        imageUrl: '/assets/video/businesscards.png',
-        videoUrl: '/assets/video/business-card.mp4',
+        title: "Business Cards",
+        imageUrl: "/assets/video/businesscards.png",
+        videoUrl: "/assets/video/business-card.mp4",
       },
       {
         id: 2,
         icon: IoIosWatch,
-        title: 'Wearables',
-        imageUrl: '/assets/video/wearables.png',
-        videoUrl: '/assets/video/wearables.mp4',
+        title: "Wearables",
+        imageUrl: "/assets/video/wearables.png",
+        videoUrl: "/assets/video/wearables.mp4",
       },
       {
         id: 3,
         icon: GrTransaction,
-        title: 'Transactions',
-        imageUrl: '/assets/video/transactions.png',
-        videoUrl: '/assets/video/transactions.mp4',
+        title: "Transactions",
+        imageUrl: "/assets/video/transactions.png",
+        videoUrl: "/assets/video/transactions.mp4",
       },
     ],
-    [],
+    []
   );
 
   const featuresInfoRight = useMemo(
@@ -60,31 +60,31 @@ const ConectMobileSection = () => {
       {
         id: 4,
         icon: IoRestaurantOutline,
-        title: 'Restaurant/ Hospital',
-        imageUrl: '/assets/video/restaurants.png',
-        videoUrl: '/assets/video/restaurants.mp4',
+        title: "Restaurant/ Hospital",
+        imageUrl: "/assets/video/restaurants.png",
+        videoUrl: "/assets/video/restaurants.mp4",
       },
       {
         id: 5,
         icon: RiChatSettingsLine,
-        title: 'Customize',
-        imageUrl: '/assets/video/customize.png',
-        videoUrl: '/assets/video/cstomizable-crad-tapp.mp4',
+        title: "Customize",
+        imageUrl: "/assets/video/customize.png",
+        videoUrl: "/assets/video/cstomizable-crad-tapp.mp4",
       },
       {
         id: 6,
         icon: IoCodeSharp,
-        title: 'Embeddables',
-        imageUrl: '/assets/video/embeddables.png',
-        videoUrl: '/assets/video/hardware-embeddables.mp4',
+        title: "Embeddables",
+        imageUrl: "/assets/video/embeddables.png",
+        videoUrl: "/assets/video/hardware-embeddables.mp4",
       },
     ],
-    [],
+    []
   );
 
   const featuresInfoAll = useMemo(
     () => [...featuresInfoLeft, ...featuresInfoRight],
-    [featuresInfoLeft, featuresInfoRight],
+    [featuresInfoLeft, featuresInfoRight]
   );
 
   const hoverItemSelect = useCallback((value) => {
@@ -99,14 +99,14 @@ const ConectMobileSection = () => {
 
     // Prefetch video URL
     if (value?.videoUrl) {
-      fetch(value.videoUrl, { method: 'HEAD' })
+      fetch(value.videoUrl, { method: "HEAD" })
         .then((response) => {
           if (response.ok) {
             console.log(`Prefetched video: ${value.videoUrl}`);
           }
         })
         .catch((error) =>
-          console.error(`Failed to prefetch video: ${value.videoUrl}`, error),
+          console.error(`Failed to prefetch video: ${value.videoUrl}`, error)
         );
     }
   }, []);
@@ -182,15 +182,15 @@ const ConectMobileSection = () => {
 
   return (
     <SectionLayout>
-      <div className='hidden md:block'>
-        <ScrollMotionEffect effect='fade-right' duration='2000'>
-          <h2 className='text-xl font-normal leading-none text-center text-stone-950 md:text-4xl'>
+      <div className="hidden md:block">
+        <ScrollMotionEffect effect="fade-right" duration="2000">
+          <h2 className="text-xl font-normal leading-none text-center text-stone-950 md:text-4xl">
             <strong>Hardware</strong> built to connect
             <br /> you with your <strong>customers</strong>.
           </h2>
         </ScrollMotionEffect>
-        <ScrollMotionEffect effect='fade-left' duration='2000'>
-          <p className='text-lg text-stone-950 text-center mt-4 max-w-[700px] mx-auto '>
+        <ScrollMotionEffect effect="fade-left" duration="2000">
+          <p className="text-lg text-stone-950 text-center mt-4 max-w-[700px] mx-auto ">
             Attach your Everything Link to devices to route customers where they
             need to go, keeping everything organized and making it easier to
             generate leads, gather reviews, take payments, offer promotions, and
@@ -199,23 +199,23 @@ const ConectMobileSection = () => {
         </ScrollMotionEffect>
       </div>
 
-      <div className='hidden md:block max-w-[400px] h-[520px] mx-auto mt-8'>
+      <div className="hidden md:block max-w-[400px] h-[520px] mx-auto mt-8">
         {hoverImgUrl ? (
           <motion.div
             key={hoverImgUrl}
-            className='mx-auto'
+            className="mx-auto"
             animate={animationProps}
             transition={
               hoveredItem === 5 && !isDelayed
                 ? {
                     duration: 1,
-                    ease: 'easeInOut',
+                    ease: "easeInOut",
                     times: [0, 0.1, 0.5, 1],
                     repeat: false,
                   }
                 : {
                     duration: 2,
-                    ease: 'easeInOut',
+                    ease: "easeInOut",
                     times: [0, 0.2, 0.5, 0.8, 1],
                     repeat: false,
                     repeatDelay: 1,
@@ -228,11 +228,11 @@ const ConectMobileSection = () => {
                 loop
                 muted
                 playsInline
-                src={'/assets/video/customizable-video.mp4'}
-                className='mx-auto'
+                src={"/assets/video/customizable-video.mp4"}
+                className="mx-auto"
                 width={160}
                 height={80}
-                preload='metadata'
+                preload="metadata"
                 // loading='lazy'
               />
             ) : (
@@ -240,8 +240,8 @@ const ConectMobileSection = () => {
                 width={180}
                 height={120}
                 src={hoverImgUrl}
-                alt='Link icon'
-                className='mx-auto'
+                alt="Link icon"
+                className="mx-auto"
               />
             )}
           </motion.div>
@@ -250,40 +250,40 @@ const ConectMobileSection = () => {
             animate={animationProps}
             transition={{
               duration: 2,
-              ease: 'easeInOut',
+              ease: "easeInOut",
               times: [0, 0.2, 0.5, 0.8, 1],
               repeat: false,
               repeatDelay: 1,
             }}
           >
             <Image
-              width={180}
+              width={160}
               height={120}
-              src={'/assets/video/transactions.png'}
-              alt='Link icon'
-              className='mx-auto'
+              src={"/assets/site-logo/chipicon.png"}
+              alt="Link icon"
+              className="mx-auto"
             />
           </motion.div>
         )}
       </div>
 
-      <div className='md:hidden max-w-[400px] h-[230px] mx-auto mt-[-60px]'>
+      <div className="md:hidden max-w-[400px] h-[230px] mx-auto mt-[-60px]">
         {hoverImgUrl ? (
           <motion.div
             key={hoverImgUrl}
-            className='mx-auto'
+            className="mx-auto"
             animate={animationMobileProps}
             transition={
               hoveredItem === 5 && !isDelayed
                 ? {
                     duration: 1,
-                    ease: 'easeInOut',
+                    ease: "easeInOut",
                     times: [0, 0.1, 0.5, 1],
                     repeat: false,
                   }
                 : {
                     duration: 2,
-                    ease: 'easeInOut',
+                    ease: "easeInOut",
                     times: [0, 0.2, 0.5, 0.8, 1],
                     repeat: false,
                     repeatDelay: 1,
@@ -293,12 +293,12 @@ const ConectMobileSection = () => {
             {hoveredItem === 5 ? (
               <video
                 autoPlay
-                src={'/assets/video/customizable-video.mp4'}
+                src={"/assets/video/customizable-video.mp4"}
                 muted
-                className='mx-auto'
+                className="mx-auto"
                 width={100}
                 height={60}
-                preload='metadata'
+                preload="metadata"
                 // loading='lazy'
               />
             ) : (
@@ -306,94 +306,94 @@ const ConectMobileSection = () => {
                 width={120}
                 height={100}
                 src={hoverImgUrl}
-                alt='Link icon'
-                className='mx-auto'
+                alt="Link icon"
+                className="mx-auto"
               />
             )}
           </motion.div>
         ) : (
-          ''
+          ""
         )}
       </div>
 
-      <div className='grid grid-cols-1 md:grid-cols-3 items-center gap-4 md:gap-14 mt-[-80px] md:mt-[-350px]'>
-        <div className='hidden md:flex flex-col justify-end gap-10 md:gap-28 mx-auto mr-[-2px]'>
+      <div className="grid grid-cols-1 md:grid-cols-3 items-center gap-4 md:gap-14 mt-[-80px] md:mt-[-350px]">
+        <div className="hidden md:flex flex-col justify-end gap-10 md:gap-28 mx-auto mr-[-2px]">
           {featuresInfoLeft.map((el) => (
             <div
               key={el.id}
-              className={`p-2 bg-white rounded-full w-28 h-28 flex flex-col items-center justify-center border border-[#737373] ${hoveredItem === el.id ? 'shadow-xl' : ''}`}
+              className={`p-2 bg-white rounded-full w-28 h-28 flex flex-col items-center justify-center border border-[#737373] ${hoveredItem === el.id ? "shadow-xl" : ""}`}
               onMouseEnter={() => hoverItemSelect(el)}
             >
-              <el.icon className='w-6 h-6 text-[#737373]' />
-              <h2 className='mt-2 text-base text-[#737373] text-center font-normal leading-5'>
+              <el.icon className="w-6 h-6 text-[#737373]" />
+              <h2 className="mt-2 text-base text-[#737373] text-center font-normal leading-5">
                 {el.title}
               </h2>
             </div>
           ))}
         </div>
 
-        <div className='relative z-50 flex justify-center mx-auto'>
+        <div className="relative z-50 flex justify-center mx-auto">
           <motion.div
             key={key}
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 2 }}
-            className='h-[280px] md:h-[600px] flex justify-center mx-auto relative z-50'
+            className="h-[280px] md:h-[600px] flex justify-center mx-auto relative z-50"
           >
             <video
               ref={videoRef}
-              src={hoverVideoUrl || '/assets/video/cstomizable-crad-tapp.mp4'}
+              src={hoverVideoUrl || "/assets/video/cstomizable-crad-tapp.mp4"}
               muted
-              preload='metadata'
-              className='mx-auto'
+              preload="metadata"
+              className="mx-auto"
               width={200}
               height={50}
-              loading='lazy'
+              loading="lazy"
               playsInline
-              poster='/assets/video/mobile.png'
+              poster="/assets/video/mobile.png"
             />
           </motion.div>
         </div>
 
-        <div className='hidden md:flex flex-col justify-start gap-10 md:gap-28 mx-auto bg-white ml-[-2px]'>
+        <div className="hidden md:flex flex-col justify-start gap-10 md:gap-28 mx-auto bg-white ml-[-2px]">
           {featuresInfoRight.map((el) => (
             <div
               key={el.id}
-              className={`p-2 bg-white rounded-full w-28 h-28 flex flex-col items-center justify-center border border-[#737373] ${hoveredItem === el.id ? 'shadow-xl' : ''}`}
+              className={`p-2 bg-white rounded-full w-28 h-28 flex flex-col items-center justify-center border border-[#737373] ${hoveredItem === el.id ? "shadow-xl" : ""}`}
               onMouseEnter={() => hoverItemSelect(el)}
             >
-              <el.icon className='w-6 h-6 text-[#737373]' />
-              <h2 className='mt-1 text-sm leading-5 text-[#737373] text-center font-normal'>
+              <el.icon className="w-6 h-6 text-[#737373]" />
+              <h2 className="mt-1 text-sm leading-5 text-[#737373] text-center font-normal">
                 {el.title}
               </h2>
             </div>
           ))}
         </div>
 
-        <div className='grid justify-center grid-cols-3 mx-auto mt-3 bg-white gap-x-5 gap-y-3 md:hidden'>
+        <div className="grid justify-center grid-cols-3 mx-auto mt-3 bg-white gap-x-5 gap-y-3 md:hidden">
           {featuresInfoAll.map((el) => (
             <div
               key={el.id}
-              className={`p-2 bg-white rounded-full size-20 flex flex-col items-center justify-center border border-[#737373] ${hoveredItem === el.id ? 'shadow-xl' : ''}`}
+              className={`p-2 bg-white rounded-full size-20 flex flex-col items-center justify-center border border-[#737373] ${hoveredItem === el.id ? "shadow-xl" : ""}`}
               onMouseEnter={() => hoverItemSelect(el)}
             >
-              <el.icon className='size-4 text-[#737373]' />
-              <h2 className='mt-1 text-xs leading-2 text-[#737373] text-center font-normal'>
+              <el.icon className="size-4 text-[#737373]" />
+              <h2 className="mt-1 text-xs leading-2 text-[#737373] text-center font-normal">
                 {el.title}
               </h2>
             </div>
           ))}
         </div>
       </div>
-      <div className='mt-10 md:hidden'>
-        <ScrollMotionEffect effect='fade-up' duration='2000'>
-          <h2 className='text-xl font-normal leading-none text-center text-stone-950 md:text-4xl'>
+      <div className="mt-10 md:hidden">
+        <ScrollMotionEffect effect="fade-up" duration="2000">
+          <h2 className="text-xl font-normal leading-none text-center text-stone-950 md:text-4xl">
             <strong>Hardware</strong> built to connect
             <br /> you with your <strong>customers</strong>.
           </h2>
         </ScrollMotionEffect>
-        <ScrollMotionEffect effect='fade-up' duration='2000'>
-          <p className='text-lg text-stone-950 text-center mt-4 max-w-[700px] mx-auto '>
+        <ScrollMotionEffect effect="fade-up" duration="2000">
+          <p className="text-lg text-stone-950 text-center mt-4 max-w-[700px] mx-auto ">
             Attach your Everything Link to devices to route customers where they
             need to go, keeping everything organized and making it easier to
             generate leads, gather reviews, take payments, offer promotions, and
