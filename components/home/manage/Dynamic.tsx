cb4f1b043154774ec7ba22ab0e274a28e788b63e
Faisal Ahamed Rahad <155021870+rahad-dev24@users.noreply.github.com>
2025-01-01 13:59:31 +0600

    fixed team issue

--- components/home/manage/Dynamic.tsx
+++ components/home/manage/Dynamic.tsx
@@ -9,20 +9,38 @@ const Dynamic = () => {
       duration="800"
       delay={300}
       className={
-        "max-w-[330px] sm:max-w-[390px] h-full  lg:max-w-full md:pt-10 lg:pt-0 m-auto bg-[#ffffff] rounded-2xl p-6 flex flex-col justify-between items-center"
+        "max-w-[330px] sm:max-w-[390px] h-full  lg:max-w-full md:pt-10  m-auto bg-[#ffffff] rounded-2xl p-6 flex flex-col justify-around items-center"
       }
     >
-      <Image
+      {/* <Image
         src="/assets/home/manage/qr.png"
         alt="qr"
         width={600}
         height={600}
         className="text-center md:scale-125 relative md:translate-y-20
-        md:pb-20 lg:scale-110 lg:translate-y-16 lg:pt-10 lg:pb-12 xl:scale-[1.20] 2xl:scale-150 xl:pb-16 xl:translate-y-16 w-60"
+        md:pb-20 lg:scale-110 lg:translate-y-16 lg:pt-10 lg:pb-12 xl:scale-[1.20] 2xl:scale-150 xl:pb-16 xl:translate-y-16 w-64"
         quality={100}
       />
 
-      <div className="w-full lg:w-5/6 2xl:w-4/6 m-auto mt-5 md:mt-24">
+      <div className="w-full  m-auto mt-5 md:mt-24">
+        <h3 className="font-semibold text-lg text-center pb-1">Dynamic QR</h3>
+
+        <h4 className="text-base text-center text-gray-400">
+          Update your Smartsite and your QR infrastructure updates
+          automatically.
+        </h4>
+      </div> */}
+      <div className="w-fit h-auto lg:h-[150px] lg:scale-105 xl:h-[300px]  -z-50 flex justify-center items-center">
+      <Image
+        src="/assets/home/manage/qr.png"
+        alt="qr"
+        width={600}
+        height={600}
+        className=" w-full h-auto"
+        quality={100}
+      /></div>
+
+      <div className="">
         <h3 className="font-semibold text-lg text-center pb-1">Dynamic QR</h3>
 
         <h4 className="text-base text-center text-gray-400">
--- components/home/manage/Team.tsx
+++ components/home/manage/Team.tsx
@@ -9,7 +9,7 @@ const Team = () => {
       duration="800"
       delay={300}
       className={
-        "xs:w-[304px] ms:w-[330px] sm:w-[390px] m-auto md:w-full  bg-[#ffffff] rounded-2xl p-6 lg:col-span-2 self-stretch flex flex-col justify-between items-center overflow-hidden "
+        "xs:w-[304px] ms:w-[330px] sm:w-[390px] m-auto md:w-full bg-[#ffffff] rounded-2xl p-6 lg:col-span-2 self-stretch flex flex-col justify-between items-center overflow-hidden "
       }
     >
       {/* <Image
@@ -27,7 +27,7 @@ const Team = () => {
         playsInline
         preload="auto"
         style={{ display: "block" }}
-        className="h-[300px]  scale-110 lg:scale-125 lg:translate-y-8 xl:scale-150 xl:translate-y-0 -z-50 "
+        className="h-[200px] scale-150 lg:h-[250px] lg:scale-125 xl:h-[330px] xl:scale-150 -z-50 "
       >
         <source
           src="/assets/home/manage/manageTheEntireTeam.mp4"
@@ -36,12 +36,12 @@ const Team = () => {
         Your browser does not support the video tag.
       </video>
 
-      <div className="w-full lg:w-4/6 m-auto flex-grow pt-5">
-        <h3 className="font-semibold text-lg text-center z-50 mb-1">
+      <div className="w-full lg:w-4/6 m-auto flex-grow">
+        <h3 className="font-semibold text-lg text-center z-50">
           Manage The Entire Team
         </h3>
 
-        <h4 className="text-lg text-center text-gray-400 max-w-full flex-grow">
+        <h4 className="text-base text-center text-gray-400 max-w-full flex-grow">
           One dashboard to create,deploy, and manage your companies network.
         </h4>
         {/* <h5 className="bg-clip-text bg-transparent text-transparent ">
