a70e5545c2346001b1c688cb7d99252ba92e9354
rahad-dev24 <rahad.dev24@gmail.com>
2025-01-14 13:10:12 +0600

    Create page.tsx

---
+++ app/account-deletion/page.tsx
@@ -0,0 +1,11 @@
+import ConectMobileSection from "@/components/hardware/ConectMobileSection";
+
+const page = () => {
+  return (
+    <div className="container py-16">
+      <ConectMobileSection />
+    </div>
+  );
+};
+
+export default page;
