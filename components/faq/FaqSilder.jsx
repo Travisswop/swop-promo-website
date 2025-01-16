e4c7c8cb63e8a1a50c9c09e301373f9549d45a33
rahad-dev24 <rahad.dev24@gmail.com>
2025-01-16 14:06:03 +0600

    added youtube video

--- components/faq/FaqSilder.jsx
+++ components/faq/FaqSilder.jsx
@@ -12,26 +12,30 @@ import Image from "next/image";
 
 const slidesData = [
   {
-    src: "/assets/faq/faq-video.jpg",
-    alt: "Swop QR Code",
-    title: "How to Activate SWOP",
+    src: "https://www.youtube.com/embed/iiBDBhY-TMg",
+    alt: "Microsite Tutorial",
+    title: "Microsite Tutorial",
   },
   {
-    src: "/assets/faq/faq-video.jpg",
-    alt: "Swop QR Code",
-    title: "How to SWOP connect an Android",
-  },
-  { src: "/assets/faq/faq-video.jpg", alt: "Swop QR Code", title: "SWOP Pro" },
-  {
-    src: "/assets/faq/faq-video.jpg",
-    alt: "Swop QR Code",
-    title: "How to Activate SWOP",
+    src: "https://www.youtube.com/embed/ycGsDI_W4Rg",
+    alt: "How to use bypass",
+    title: "How to use bypass",
   },
   {
-    src: "/assets/faq/faq-video.jpg",
-    alt: "Swop QR Code",
-    title: "How to SWOP connect an Android",
+    src: "https://www.youtube.com/embed/Vb2GHXDQuYo",
+    alt: "How to create custom QR code",
+    title: "How to create custom QR code",
   },
+  {
+    src: "https://www.youtube.com/embed/EHS7L8vljLo",
+    alt: "Swop NFC tutorial",
+    title: "Swop NFC tutorial",
+  },
+  // {
+  //   src: "/assets/faq/faq-video.jpg",
+  //   alt: "Swop QR Code",
+  //   title: "How to SWOP connect an Android",
+  // },
 ];
 
 const FaqSlider = () => {
@@ -48,11 +52,11 @@ const FaqSlider = () => {
         },
         768: {
           slidesPerView: 2,
-          spaceBetween: 30,
+          spaceBetween: 10,
         },
         1024: {
           slidesPerView: 4,
-          spaceBetween: 40,
+          spaceBetween: 10,
         },
       }}
       className="mySwiper"
@@ -60,14 +64,22 @@ const FaqSlider = () => {
       {slidesData?.map((slide, index) => (
         <SwiperSlide key={index}>
           <div className="flex flex-col items-center w-[1700px] h-auto">
-            <Image
+            {/* <Image */}
+            {/*   src={slide.src} */}
+            {/*   alt={slide.alt} */}
+            {/*   width={800} */}
+            {/*   height={200} */}
+            {/*   layout="responsive" */}
+            {/*   objectFit="cover" */}
+            {/*   quality={100} */}
+            {/* /> */}
+            <iframe
               src={slide.src}
-              alt={slide.alt}
-              width={800}
+              allowFullScreen
+              width={260}
               height={200}
-              layout="responsive"
-              objectFit="cover"
-              quality={100}
+              loading="lazy"
+              className={"rounded-xl"}
             />
             <h2 className="text-stone-950 font-normal text-lg md:text-2xl text-center mt-4 mb-12">
               {slide.title}
